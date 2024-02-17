cb85bad182160468f39ba6105046df7968b8b76f
LoveKaushik2001 <lovekaushik2001@gmail.com>
2024-02-17 18:53:34 +0530

    cors config updated

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: ["https://drink-cart.vercel.app/"],
+    origin: "https://drink-cart.vercel.app/",
     methods: ["POST", "GET"],
     credentials: true,
   })
