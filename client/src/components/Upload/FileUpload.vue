8eb6444c4c6807f13887d2b3fafc6a6be9647618
LoveKaushik2001 <lovekaushik2001@gmail.com>
2024-07-27 14:58:13 +0530

    data upload from url added

--- client/src/components/Upload/FileUpload.vue
+++ client/src/components/Upload/FileUpload.vue
@@ -12,12 +12,102 @@ const customerDetail = ref({} as {
     discountAvailed: string
 })
 const jsonData = ref([] as any[]);
-const readFile = (event: Event) => {
+const loaderVisible = ref(false);
+
+const readSheetFile = async () => {
+    loaderVisible.value = true;
+    const fileUrl = 'https://docs.google.com/spreadsheets/d/1Rd7Io_lQCCbr9DGeFBH7mNNioB-CrkOV5WXU4qfA2s8/export?format=xlsx';
+    try {
+        const response = await fetch(fileUrl);
+        if (!response.ok) throw new Error('Network response was not ok.');
+        const blob = await response.blob();
+        const reader = new FileReader();
+        reader.onload = async (e: any) => {
+            const workbook = XLSX.read(e.target.result, { type: 'array' });
+            await processWorkbook(workbook);
+            loaderVisible.value = false;
+        };
+        reader.readAsArrayBuffer(blob);
+    } catch (error) {
+        loaderVisible.value = false;
+        alert('OOPS!!!, Something went wrong.')
+        console.error('Error fetching or processing the file:', error);
+    }
+}
+
+const processWorkbook = async (workbook: XLSX.WorkBook) => {
+    const customerSheetName = workbook.SheetNames.find((ele) => ele.includes('customersite'))
+    const deliverySheetName = workbook.SheetNames.find((ele) => ele.includes('deliverysite'))
+    const packagingSheetName = workbook.SheetNames.find((ele) => ele.includes('packagingsite'))
+    if (customerSheetName && deliverySheetName) {
+        const customerInfo = workbook.Sheets[customerSheetName];
+        const deliveryInfo = workbook.Sheets[deliverySheetName];
+        const customerInfoData = XLSX.utils.sheet_to_json(customerInfo, { raw: false });
+        const deliveryInfoData = XLSX.utils.sheet_to_json(deliveryInfo, { raw: false });
+        await storeData(customerInfoData, 'customerInfo');
+        await storeData(deliveryInfoData, 'deliveryInfo');
+    }
+    if (packagingSheetName) {
+        const packageInfo = workbook.Sheets[packagingSheetName];
+        const packageInfoData = XLSX.utils.sheet_to_json(packageInfo, { raw: false });
+        await storeData(packageInfoData, 'route');
+    }
+};
+const storeData = async (jsonData: any[], name: string) => {
+    jsonData.map(row => {
+        Object.keys(row).forEach(key => {
+            if (typeof row[key] === 'number' && XLSX.SSF.is_date(key)) {
+                row[key] = XLSX.SSF.parse_date_code(row[key]);
+            }
+            const nestedFields = key.split('_');
+            if (nestedFields?.length === 2) {
+                if (!row[nestedFields[0]]) {
+                    row[nestedFields[0]] = {};
+                }
+                row[nestedFields[0]][nestedFields[1]] = row[key];
+                delete row[key];
+            }
+        })
+    })
+    try {
+        if (name?.toLowerCase()?.includes?.('customerinfo')) {
+            const result = await addCustomerInfo(jsonData, true);
+            const initExtraInfoData = [] as any;
+            jsonData.forEach(data => {
+                initExtraInfoData.push({
+                    customerId: data.customerId,
+                    customerName: data.customerName,
+                    address: data.address,
+                    routeBoy: '',
+                    timeStamp: '',
+                    distanceFromLastPoint: 0,
+                    coords: { lat: '', lng: '' },
+                    origin: { lat: '', lng: '' },
+                    lastPoint: { lat: '', lng: '' },
+                    bottlesDelivered: 0,
+                    bottlesCollected: 0,
+                    bottlesRemaining: 0
+                })
+            })
+            await initExtraInfo(initExtraInfoData);
+        } else if (name?.toLowerCase().includes('route') || name?.toLowerCase().includes('package')) {
+            const result = await addRouteSheet(jsonData);
+        }
+        else {
+            const result = await addDeliverySheet(jsonData);
+        }
+        localStorage.clear();
+    } catch (error) {
+        alert('Something went wrong try again');
+    }
+}
+
+const readFile = async (event: Event) => {
     const input = event.target as HTMLInputElement;
     if (input.files && input?.files?.length > 0) {
         file.value = input.files[0];
         const reader = new FileReader();
-        reader.onload = (e: any) => {
+        reader.onload = async (e: any) => {
             try {
                 const data = new Uint8Array(e.target.result);
                 const workbook = XLSX.read(data, { type: 'array' });
@@ -53,6 +143,7 @@ const readFile = (event: Event) => {
 }
 
 const addData = async (isUploaded: boolean) => {
+    loaderVisible.value = true;
     const dataToBeSaved = isUploaded ? jsonData.value : [customerDetail.value];
     try {
         if (file.value && file.value?.name?.toLowerCase().includes?.('customerinfo')) {
@@ -81,16 +172,19 @@ const addData = async (isUploaded: boolean) => {
         else {
             const result = await addDeliverySheet(dataToBeSaved);
         }
+        loaderVisible.value = false
         file.value = null;
         alert('Data Added');
         localStorage.clear();
     } catch (error) {
         file.value = null;
+        loaderVisible.value = false;
         alert('Something went wrong try again');
     }
 }
 </script>
 <template>
+    <div v-if="loaderVisible" class="loader active"></div>
     <DownloadExtraInfo />
     <div class="flex items-center justify-center p-12">
         <div class="mx-auto w-full max-w-[550px] bg-white">
@@ -126,39 +220,9 @@ const addData = async (isUploaded: boolean) => {
                 </div>
             </form>
         </div>
-    </div>
-
-    <div class="flex items-center justify-center p-12">
-        <div class="mx-auto w-full max-w-[550px] bg-white">
-            <form @submit.prevent="addData(false)" class="form">
-                <label class="mb-5 block text-xl font-semibold text-[#07074D]">
-                    Enter customers details:
-                </label>
-                <p>
-                    Customer Id*
-                    <input type="text" required placeholder="Id like 0001" v-model="customerDetail.customerId" />
-                </p>
-                <p>
-                    Customer Name*
-                    <input type="text" required placeholder="Name" v-model="customerDetail.customerName" />
-                </p>
-                <p>
-                    Customer Phone Number*
-                    <input type="text" required placeholder="phone number" v-model="customerDetail.phoneNumber" />
-                </p>
-                <p>
-                    Customer Address*
-                    <input type="text" required placeholder="address" v-model="customerDetail.address" />
-                </p>
-                <p>
-                    Discount Availed
-                    <input type="text" placeholder="discount" v-model="customerDetail.discountAvailed" />
-                </p>
-                <button type="submit"
-                    :class="['bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded']">Add
-                    Customer</button>
-            </form>
-        </div>
+        <button @click="readSheetFile"
+            :class="['bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-3 px-4 border border-blue-500 hover:border-transparent rounded w-full rounded-md']">Read
+            File</button>
     </div>
 </template>
 <style>
@@ -166,5 +230,34 @@ const addData = async (isUploaded: boolean) => {
     border: dashed black 1px;
     width: 200px;
     padding: 20px;
+}
+
+.loader {
+    border: 16px solid #f3f3f3;
+    border-top: 16px solid #3498db;
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1000;
+    /* Ensure it appears above other content */
+}
+
+.loader.active {
+    display: block;
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
 }
 </style>
